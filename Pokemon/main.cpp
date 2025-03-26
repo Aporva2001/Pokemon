254ab1d8ed26e9ae2dc11a33b710190e1a469588
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-03-26 12:18:08 +0530

    Starting with main function

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
 
 int main() {
 
+    cout<<"Enter your name"<<endl;
+    string player_name;
+    cin>> player_name;
+    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
     return 0;
 }
