483e5b79bbb81754bd96d553cb78e0c98409ae17
Aporva2001 <apurvgoyal2192001@gmail.com>
2025-03-31 13:32:51 +0530

    Controlling the Battle Flow

--- Pokemon/BattleManager.cpp
+++ Pokemon/BattleManager.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"BattleManager.hpp"
+#include"Pokemon.hpp"
 using namespace std;
 
     void BattleManager :: battle(Pokemon &playerPokemon, Pokemon &wildPokemon) {
@@ -20,36 +21,48 @@ using namespace std;
         }
     }
 
-    void BattleManager :: startBattle(Player &player, Pokemon &wildPokemon){
-        cout << "A wild " << wildPokemon.name << " appeared!\n";
-        battle(player.chosenPokemon, wildPokemon);
-    }
+        void BattleManager::startBattle(Player &player, Pokemon &wildPokemon) {
+            battleState.playerPokemon = &player.chosenPokemon;
+            battleState.wildPokemon = &wildPokemon;
+            battleState.playerTurn = true;  // Player starts first
+            battleState.battleOngoing = true;
+        
+            std::cout << "A wild " << wildPokemon.name << " appeared!\\n";
+            battle();
+        }
 
-    void BattleManager :: battle(Pokemon &playerPokemon, Pokemon &wildPokemon){
-        while (!playerPokemon.isFainted() && !wildPokemon.isFainted()) {
-            // Player attacks first
-            playerPokemon.attack(wildPokemon);
-    
-            // Check if wild Pokémon fainted
-            if (!wildPokemon.isFainted()) {
-                // Wild Pokémon attacks back
-                wildPokemon.attack(playerPokemon);
+    void BattleManager :: battle(){
+        while (battleState.battleOngoing) {
+            if (battleState.playerTurn) {
+                // Player's turn to attack
+                battleState.playerPokemon->attack(*battleState.wildPokemon);
+            } else {
+                // Wild Pokémon's turn to attack
+                battleState.wildPokemon->attack(*battleState.playerPokemon);
             }
     
-            // Pause to show the result of each turn
+            // Update the battle state after the turn
+            updateBattleState();
+    
+            // Switch turns
+            battleState.playerTurn = !battleState.playerTurn;
+    
             Utility::waitForEnter();
         }
-        // Determine and display the outcome of the battle
-        handleBattleOutcome(playerPokemon, playerPokemon.isFainted());
+    
+        handleBattleOutcome();
     }
-    void BattleManager :: handleBattleOutcome(Player &player, bool playerWon){
-        if(playerWon){
-            cout<<player.chosenPokemon.name<<" is victorious. Keep an eye on your Pokemon's health";
+    void BattleManager :: handleBattleOutcome(){
+        if (battleState.playerPokemon->isFainted()) {
+            cout << battleState.playerPokemon->name << " has fainted! You lose the battle.\\n";
+        } else {
+            cout << "You defeated the wild " << battleState.wildPokemon->name << "!\\n";
         }
-        else{
-            cout<<"Oh no! "<< player.chosenPokemon.name<<" fainted! You need to visit the PokeCenter.";
+    }
+    void BattleManager::updateBattleState() {
+        if (battleState.playerPokemon->isFainted()) {
+            battleState.battleOngoing = false;
+        } else if (battleState.wildPokemon->isFainted()) {
+            battleState.battleOngoing = false;
         }
-        Utility:: waitForEnter();
-        cout<<"Game over \n";
-        
     }
--- Pokemon/BattleManager.hpp
+++ Pokemon/BattleManager.hpp
@@ -1,7 +1,13 @@
-class BattleManager{
-    public:
-        void startBattle(Player &player, Pokemon &wildPokemon);
-    private:
-        void battle(Pokemon &playerPokemon, Pokemon &wildPokemon);
-        bool handleBattleOutcome(Player &player, bool playerWon);
+#include "BattleState.hpp"
+#include "Player.hpp"
+
+class BattleManager {
+public:
+    void startBattle(Player &player, Pokemon &wildPokemon);
+private:
+    BattleState battleState;  // New BattleState object to track the battle
+
+    void battle();
+    void handleBattleOutcome();
+    void updateBattleState(); // Method to update the battle state after each turn
 };
